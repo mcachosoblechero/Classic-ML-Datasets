d9105865bbcd6099d39a03e54213f1aadc7f97f3
Miguel Cacho Soblechero <miguelcachosoblechero@gmail.com>
2023-01-02 13:36:47 -0500

    Create feature extraction function

--- Titanic/utils/preprocessing_tools.py
+++ Titanic/utils/preprocessing_tools.py
@@ -5,3 +5,17 @@ def perc_missing_vals(df):
     """
     missing_vals_train = df.isnull().sum() / df.shape[0] * 100
     print(missing_vals_train.sort_values(ascending=False))
+
+def titanic_feature_extraction(df):
+    """
+    Perform the feature extraction for the Titanic dataset, as discussed in the EDA notebook
+    :param df: Dataframe with all records
+    :return: Dataframe with new features
+    """
+    # Extract whether the passenger has a cabin
+    df['hasCabin'] = df.Cabin.notnull().apply(lambda x: 1 if x==True else 0)
+    # Extract the passenger cabin letter
+    df['cabinLetter'] = df.loc[df.Cabin.notnull()].Cabin.str[0]
+    # Extract the number of cabins
+    df['numCabins'] = df.loc[df.Cabin.notnull()].Cabin.str.split(" ").apply(len)
+    return df
